75ae8c21a5a0940dac63f97ab852a0b72b1ca1df
RomeoEncina <borjarombet@gmail.com>
2025-07-05 14:18:29 +0800

    Update Premium2.lua

--- Premium2.lua
+++ Premium2.lua
@@ -56,6 +56,6 @@ if script_id then
         local auth_status = auth(script_id)
         repeat task.wait() until auth_status.validated
     end
-    if not premium then script_key = TePzKWviutPzyNmGRKrtGlczDljLJrMM or nil; premium = true; is_premium = true end
+    if not premium then script_key = "TePzKWviutPzyNmGRKrtGlczDljLJrMM" or nil; premium = true; is_premium = true end
     _function.load("https://api.luarmor.net/files/v4/loaders/" .. script_id .. ".lua")
 end
